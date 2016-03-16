a0203874ecc8f507e1ab6604def1461a0409abff
qinlin <im@mes0.com>
2016-03-16 17:43:54 +0800

    Site updated: 2016-03-16 17:43:54

--- js/src/utils.js
+++ js/src/utils.js
@@ -33,6 +33,26 @@ NexT.utils = NexT.$u = {
     });
   },
 
+  lazyLoadPostsImages: function () {
+    $('#posts').find('img').lazyload({
+      placeholder: '/images/loading.gif',
+      effect: 'fadeIn'
+    });
+  },
+
+  registerBackToTop: function () {
+    var THRESHOLD = 50;
+
+    $top = $('.back-to-top');
+    $(window).on('scroll', function () {
+      $top.toggleClass('back-to-top-on', document.body.scrollTop > THRESHOLD);
+    });
+
+    $top.on('click', function () {
+      $('body').velocity('scroll');
+    });
+  },
+
   /**
    * Transform embedded video to support responsive layout.
    * @see http://toddmotto.com/fluid-and-responsive-youtube-and-vimeo-videos-with-fluidvids-js/
@@ -143,7 +163,7 @@ NexT.utils = NexT.$u = {
   },
 
   displaySidebar: function () {
-    if (!this.isDesktop()) {
+    if (!this.isDesktop() || this.isPisces()) {
       return;
     }
     $('.sidebar-toggle').trigger('click');
